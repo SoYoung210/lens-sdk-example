0f63efea1f8f646a98971202abdecb47fc84f3d2
SoYoung210 <ethdud1@gmail.com>
2024-09-02 10:10:31 +0900

    add leftSlot at Button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -3,6 +3,7 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/utils/style';
+import { If } from '../utils/If';
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50',
@@ -35,13 +36,21 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  leftSlot?: React.ReactNode;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, variant, size, asChild = false, children, leftSlot, ...props }, ref) => {
     const Comp = asChild ? Slot : 'button';
     return (
-      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
+        <div className="flex items-center">
+          <If condition={leftSlot != null}>
+            <div className="flex items-center mr-2">{leftSlot}</div>
+          </If>
+          {children}
+        </div>
+      </Comp>
     );
   }
 );
